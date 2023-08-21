4f0ef9306427ad3322580aaa22be752a0abdc303
Luke Millam <luke.millam@gmail.com>
2023-08-20 17:22:47 -0700

    added weather styles

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {API_KEY} from './api/weatherApi.js'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  state = {
+    isLoading: false,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+    const { isLoading } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? (
+          <Text>Fetching Data</Text>
+    ) : (
+      <View>
+        <Text>Weather App</Text>
+        <StatusBar style="auto" />
+      </View>
+      )}
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ pages/weather.jsx
@@ -0,0 +1,7 @@
+const Weather = ({ weather, temperature}) => {
+    return(
+        <View>
+                
+        </View>
+    )
+}
