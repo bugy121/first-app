61c7f002cb068728d9743b526847e51fde79eba9
Luke Millam <luke.millam@gmail.com>
2023-08-23 19:26:04 -0700

    added different weather components

---
+++ components/weatherComponents.jsx
@@ -0,0 +1,42 @@
+export const weatherConditions = {
+    Rain: {
+        color: "blue",
+        title: "Raining",
+        icon: "weather-rain"
+    },
+    Clear: {
+        color: "orange",
+        title: "Sunny",
+        icon: "weather-sun"
+    },
+    Lightning: {
+        color: "yellow",
+        title: "Lightning",
+        icon: "weather-thunder"
+    },
+    Cloud: {
+        color: "white",
+        title: "Cloudy",
+        icon: "weather-cloudy"
+    },
+    Snow: {
+        color: "gray",
+        title: "Snow",
+        icon: "weather-snow"
+    },
+    Drizzle: {
+        color: "blue",
+        title: "Drizzle",
+        icon: "weather-drizzle"
+    },
+    Haze: {
+        color: "gray",
+        title: "Haze",
+        icon: "weather-haze:"
+    },
+    Mist: {
+        color: "blue",
+        title: "Mist",
+        icon: "weather-mist"
+    }
+};
